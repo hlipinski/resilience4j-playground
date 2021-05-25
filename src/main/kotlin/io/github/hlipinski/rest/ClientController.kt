cc040b6df430e78f2e94761dc814c24f677a4ab9
Hubert Lipinski <hubert.lipinski@allegro.pl>
2021-05-25 19:40:16 +0200

    RateLimiter added

--- src/main/kotlin/io/github/hlipinski/rest/ClientController.kt
+++ src/main/kotlin/io/github/hlipinski/rest/ClientController.kt
@@ -3,6 +3,7 @@ package io.github.hlipinski.rest
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException
 import io.github.resilience4j.circuitbreaker.CircuitBreaker
 import io.github.resilience4j.decorators.Decorators
+import io.github.resilience4j.ratelimiter.RateLimiter
 import io.github.resilience4j.retry.Retry
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -18,7 +19,8 @@ import java.util.function.Supplier
 class ClientController(
     private val simpleAppClient: SimpleAppClient,
     private val circuitBreaker: CircuitBreaker,
-    private val retry: Retry
+    private val retry: Retry,
+    private val rateLimiter: RateLimiter
 ) {
 
     var logger: Logger = LoggerFactory.getLogger(ClientController::class.java)
@@ -33,7 +35,7 @@ class ClientController(
 
         kotlin.runCatching { decoratedSupplier.get() }
             .onSuccess { return ResponseEntity.ok(it) }
-            .onFailure { logger.error(it.message) }
+            .onFailure { logger.error("${it.javaClass} ${it.message}") }
 
         return ResponseEntity.ok().build()
     }
@@ -45,10 +47,17 @@ class ClientController(
         kotlin.runCatching { decoratedSupplier.get() }
             .onSuccess { return ResponseEntity.ok(it) }
             .onFailure {
-                logger.error(it.message)
+                logger.error("${it.javaClass} ${it.message}")
                 return ResponseEntity.ok(it.message)
             }
 
         return ResponseEntity.ok().build()
+    }
+
+    @GetMapping("/rateLimiter")
+    fun getWithRateLimit(): ResponseEntity<String> {
+        val decoratedSupplier = RateLimiter.decorateSupplier(rateLimiter) { simpleAppClient.getRegular() }
+
+        return ResponseEntity.ok(decoratedSupplier.get())
     }
 }
